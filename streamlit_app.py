4a3e7912d9907e2d9cf3b3a7e520700e31223eff
CyberDiaper <aarnevking@gmail.com>
2025-01-04 21:55:38 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+import pandas as pd
 st.title('Machine Learning App')
 
-st.write('This is app builds a machine learning model!')
+st.info('This is app builds a machine learning model!')
+
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/refs/heads/master/penguins.csv')
+  df 
